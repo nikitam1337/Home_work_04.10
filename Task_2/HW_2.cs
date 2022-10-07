f26b7fd30becf3160d88a42b26bf626476ca7e0a
nikitam1337 <nikitam.l@mail.ru>
2022-10-07 19:14:02 +0300

    Добавил 3-е ДЗ

---
+++ Task_2/HW_2.cs
@@ -0,0 +1,46 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+int number;
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+int x1 = 100;
+int x2 = 1000;
+int n = 10;
+while (true)
+{
+    if (-100 < number && number < 100)
+    {
+        Console.WriteLine($"{number} -> третьей цифры нет");
+        break;
+    }
+    
+
+    if (x1 <= number && number < x2)
+    {
+        int num3 = number % n;
+        Console.WriteLine($"{number} -> {num3}");
+        break;
+    }
+    else
+    {
+
+    }
+}
+
+// if // ввести х1 и х2 и умножать их на 10, после каждой проверки, если она не выполнилась и првоерять заново
+//break;
+
+
+
+
+//Math.Pow (число, степень)
+//double nam = Math.Pow (10, 2);
---
+++ Task_3/HW_3.cs
@@ -0,0 +1,58 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+int number;
+while (true)
+{
+    Console.Write("Введите номер дня недели: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine($"Ошибка ввода!");
+}
+
+if (number < 1)
+{
+    Console.WriteLine("В неделе всего 7 дней, перезапустите программу");
+}
+
+if (number > 7)
+{
+    Console.WriteLine("В неделе всего 7 дней, перезапустите программу");
+}
+
+if (number == 1)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+
+if (number == 2)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+
+if (number == 3)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+
+if (number == 4)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+
+if (number == 5)
+{
+    Console.WriteLine($"{number} -> Нет");
+}
+
+if (number == 6)
+{
+    Console.WriteLine($"{number} -> Да");
+}
+
+if (number == 7)
+{
+    Console.WriteLine($"{number} -> Да");
+}
